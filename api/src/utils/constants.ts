9a579994b4970705aba67b9f66afd8e894d792b7
dvikas <vikas.nice@gmail.com>
2020-12-15 20:38:33 +0530

    Remove Stripe details, will add when implement

--- api/src/utils/constants.ts
+++ api/src/utils/constants.ts
@@ -1,7 +1,4 @@
 // import { BillingFrequency } from '@prisma/client';
-import { verifyEnvironmentVariables } from './verifyEnvironmentVariables';
+// import { verifyEnvironmentVariables } from './verifyEnvironmentVariables';
 
 export const cookieDuration = 1000 * 60 * 60 * 24 * 365; // 1 year
-
-verifyEnvironmentVariables(process.env.COMMON_STRIPE_YEARLY_PLAN_ID, 'COMMON_STRIPE_YEARLY_PLAN_ID');
-verifyEnvironmentVariables(process.env.COMMON_STRIPE_MONTHLY_PLAN_ID, 'COMMON_STRIPE_MONTHLY_PLAN_ID');
